2e80951b061e36962e39324cd60d032ac3bf6cdc
mferuzz <feruztelegraph@gmail.com>
2023-07-21 20:43:03 +0500

    kliyentApi

--- src/BackendApi/apiPages/kliyentApi.js
+++ src/BackendApi/apiPages/kliyentApi.js
@@ -1,43 +1,50 @@
-import {
-    createApi,
-    fetchBaseQuery
-  } from "@reduxjs/toolkit/query/react";
-  
-  export const kliyentApi = createApi({
-    reducerPath: "kliyentApi",
-    baseQuery: fetchBaseQuery({
-      baseUrl: "https://director-2b91ca120c35.herokuapp.com/main/"
-    }),
-    endpoints: (builder) => ({
-        kliyentlar: builder.query({
-          query: () => "/get_clent"
-        }),
-        addKliyent: builder.mutation({
-          query: (task) => ({
-            url: "/add_clent",
-            method: "POST",
-            body: task
-          })
-        }),
-        updateKliyent: builder.mutation({
-          query: ({ id, ...rest }) => ({
-            url: `/update_clent/${id}`,
-            method: "PUT",
-            body: rest
-          })
-        }),
-        deleteKliyent: builder.mutation({
-          query: (id) => ({
-            url: `/delete_clent/${id}`,
-            method: "DELETE"
-          })
-        })
-      })
-  });
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-  export const {
-    useKliyentlarQuery,
-    useAddKliyentMutation,
-    useUpdateKliyentMutation,
-    useDeleteKliyentMutation
-  } = kliyentApi;
+const BASE_URL = "https://director-2b91ca120c35.herokuapp.com";
+export const kliyentApi = createApi({
+  reducerPath: "kliyentlarApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+  }),
+  endpoints: (builder) => ({
+    kliyent: builder.query({
+      query: () => ({
+        url: "/main/",
+        method: "POST",
+        body: {
+          method: "get_clent",
+          params: {},
+        },
+        headers: {
+          Authorization: "Bearer 516abb367905849725104e19556e60c7eec48cee",
+        },
+      }),
+    }),
+    addKliyent: builder.mutation({
+      query: (clent) => ({
+        url: "/add_clent",
+        method: "POST",
+        body: clent,
+      }),
+    }),
+    // updateDokonlar: builder.mutation({
+    //     query: ({ id, ...rest }) => ({
+    //         url: /update_dokon/${id},
+    //         method: "PUT",
+    //         body: rest,
+    //     }),
+    // }),
+    // deleteDokonlar: builder.mutation({
+    //     query: (id) => ({
+    //         url: /delete_dokon/${id},
+    //         method: "DELETE",
+    //     }),
+    // }),
+  }),
+});
+export const {
+  useKliyentQuery,
+  useAddKliyentMutation,
+  // useUpdateDokonlarMutation,
+  // useDeleteDokonlarMutation,
+} = kliyentApi;
--- src/page/Kliyentlar/Kliyentlar.js
+++ src/page/Kliyentlar/Kliyentlar.js
@@ -1,59 +1,48 @@
 import React, { useState } from "react";
 import "./Kliyentlar.css";
-import kliyentData from "./KliyentlarData.js";
-import { kliyentApi, useAddKliyentMutation, useDeleteKliyentMutation, useKliyentlarQuery, useUpdateKliyentMutation } from "../../BackendApi/apiPages/kliyentApi";
+// import kliyentData from "./KliyentlarData.js";
+import {
+  kliyentApi,
+  useAddKliyentMutation,
+  useDeleteKliyentMutation,
+  useKliyentQuery,
+  useKliyentlarQuery,
+  useUpdateKliyentMutation,
+} from "../../BackendApi/apiPages/kliyentApi";
 import { useNavigate } from "react-router-dom";
 
 function Kliyentlar() {
+  // ------- state ------
+  // const [name , setName ] = useState();
+  // const [phone , setPhone ] = useState();
+  // const [ xabar_berish , setXabar_berish ] = useState();
+  // const [ oxirgi_product , setOxirgi_product ] = useState();
 
-  // const {kliyentDataApi} =
+  // ------- api -------
+  const { data, error, isLoading, isSuccess } = useKliyentQuery();
+  // const [addKliyent, result] = useAddKliyentMutation();
+  // const [updateKliyent] = useUpdateKliyentMutation();
+  // const [deleteKliyent] = useDeleteKliyentMutation();
 
-  // ------- state ------ 
-  const [name , setName ] = useState();
-  const [phone , setPhone ] = useState();
-  const [ xabar_berish , setXabar_berish ] = useState();
-  const [ oxirgi_product , setOxirgi_product ] = useState();
-
-
-
-   // ------- api ------- 
-  const { kliyentApi, error, isLoading, isSuccess } = useKliyentlarQuery();
-  const [addKliyent, result] = useAddKliyentMutation();
-  const [updateKliyent] = useUpdateKliyentMutation();
-  const [deleteKliyent] = useDeleteKliyentMutation();
-
-  const handleUpdateKliyent = (e) => {
-    e.preventDefault();
-    const kliyentApi = {
-      name,
-      phone,
-      xabar_berish,
-      oxirgi_product
-    };
-    updateKliyent(kliyentApi);
-  };
-  console.log(kliyentApi);
-
+  console.log(data);
   return (
     <div className="kliyentCard">
-       <button>Kliyent qo`shish</button>
       <div className="kliyentBox">
-        {isSuccess && kliyentApi.map((element, value) => {
-          return (
-            <div className="kliyentTitle">
-              <img src={element.img} alt="" />
-              <h3 className="kliyentIsm">{element.name}</h3>
-              <h4 className="kliyentPhone">{element.phone}</h4>
+        {isSuccess &&
+          data?.result.map((item, index) => (
+            <div className="kliyentTitle" key={index}>
+              <img src={item.img} alt="" />
+              <h4 className="kliyentIsm">{item.phone}</h4>
+              <h3 className="kliyentPhone">{item.name}</h3>
               <div className="kliyentXrd">
-                <h5>{element.xabar_berish}</h5> <span>|</span>
-                <h5>{element. oxirgi_product}</h5>
+                <h5>{item.xabar_berish}</h5> <span>|</span>
+                <h5>{item.oxirgi_product}</h5>
               </div>
-              <button className="kliyentEdit" type="button" onClick={handleUpdateKliyent()}>
+              <button className="kliyentEdit" type="button">
                 Tahrirlash
               </button>
             </div>
-          );
-        })}
+          ))}
       </div>
     </div>
   );
